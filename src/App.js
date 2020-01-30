38ddab36ab5ac197e3e0c970efd7b9f04f1e8064
sgharai <sofiagharaibeh@gmail.com>
2020-01-30 14:24:49 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// prompt for player name and rps x2
+
+//switch statements for each of player 1's choices
+
+function rockPaperScissors() {
+  let player1 = {
+    name: prompt("Enter Player 1's name: "),
+    choice: prompt("Player 1, enter rock, paper or scissors!")
+  }
+  let player2 = {
+    name: prompt("Enter Player 2's name: "),
+    choice: prompt("Player 2, enter rock, paper or scissors!")
+  }
+
+  if(player1.choice === "rock") {
+    switch(player2.choice){
+      case "paper":
+        console.log(`${player2.name} wins!`);
+        break;
+      case "scissors":
+        console.log(`${player1.name} wins!`);
+        break;
+      case "rock":
+        console.log("Tie!");
+        break;
+      default:
+        alert("Please enter a valid choice.");
+    }
+  } else if(player1.choice === "paper") {
+    switch(player2.choice){
+      case "scissors":
+        console.log(`${player2.name} wins!`);
+        break;
+      case "rock":
+        console.log(`${player1.name} wins!`);
+        break;
+      case "paper":
+        console.log("Tie!")
+        break;
+      default:
+        alert("Please enter a valid choice.");
+     } 
+    } else if(player1.choice === "scissors") {
+      switch(player2.choice){
+        case "rock":
+          console.log(`${player2.name} wins!`);
+          break;
+        case "paper":
+          console.log(`${player1.name} wins!`);
+          break;
+        case "scissors":
+          console.log("Tie!")
+          break;
+        default:
+          alert("Please enter a valid choice.");
+        }
+      } else {
+        console.log("Please enter a valid choice.")
+      }
+  return null;
 }
 
-export default App;
+export default rockPaperScissors;
